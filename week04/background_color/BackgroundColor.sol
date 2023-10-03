fe0d6c40c9a3b17ceb869b5eb5dd6323c257b226
Doug Rosman <dougrosman@gmail.com>
2023-10-03 08:24:10 -0500

    add contract

---
+++ week04/background_color/BackgroundColor.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract BackgroundColor {
+
+    uint8 public red; // 0-255
+    uint8 public green; // 0-255
+    uint8 public blue; // 0-255
+
+    function setRed(uint8 _red) public {
+        red = _red;
+    }
+
+    function setGreen(uint8 _green) public {
+        green = _green;
+    }
+
+    function setBlue(uint8 _blue) public {
+        blue = _blue;
+    }
+
+    function getRed() public view returns (uint8)  {
+        return red;
+    }
+
+    function getGreen() public view returns (uint8)  {
+        return green;
+    }
+
+    function getBlue() public view returns (uint8) {
+        return blue;
+    }
+
+    // you can return multiple values from a single method, but we're
+    // not going to use this right now
+    // function getColor() public view returns (uint8, uint8, uint8) {
+    //     return (red, green, blue);
+    // }
+
+}
